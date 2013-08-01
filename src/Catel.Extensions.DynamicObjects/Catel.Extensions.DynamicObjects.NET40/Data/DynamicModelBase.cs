e905ff45b45719cb1d8ed817e5a309849ce8045c
Geert van Horrik <geert@catenalogic.com>
2013-08-01 23:04:30 +0200

    (+) CTL-139 Added project files for DynamicObjects extension

---
+++ src/Catel.Extensions.DynamicObjects/Catel.Extensions.DynamicObjects.NET40/Data/DynamicModelBase.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DynamicModelBase.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Data
+{
+    using System.Dynamic;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Dynamic model base implementing the <see cref="IDynamicMetaObjectProvider"/>.
+    /// </summary>
+    public class DynamicModelBase : ModelBase
+    {
+
+    }
+}
