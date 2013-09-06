5f4f3f127e3e93f3fa0c72d6031013fea10b0c9f
Geert van Horrik <geert@catenalogic.com>
2013-09-06 11:18:32 +0200

    (+) CTL-172 Added ScopeManager to create custom scopes for any objects

---
+++ src/Catel.Core/Catel.Core.NET40/Scoping/ScopeManager.cs
@@ -0,0 +1,174 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ScopeManager.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Scoping
+{
+    using System;
+    using System.Collections.Generic;
+    using Catel.IoC;
+    using Catel.Logging;
+    using Catel.Reflection;
+
+    /// <summary>
+    /// Custom scope manager to define a scope for a type.
+    /// </summary>
+    /// <typeparam name="T">The type to scope.</typeparam>
+    public class ScopeManager<T> : IDisposable
+        where T : class
+    {
+        #region Constants
+        private static readonly object _lock = new object();
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private static Dictionary<string, object> _instances = new Dictionary<string, object>();
+        #endregion
+
+        #region Fields
+        private readonly string _scopeName;
+        private T _scopeObject;
+        private int _refCount;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScopeManager{T}" /> class.
+        /// </summary>
+        /// <param name="scopeName">Name of the scope.</param>
+        /// <param name="createScopeFunction">The create scope function.</param>
+        protected ScopeManager(string scopeName, Func<T> createScopeFunction)
+        {
+            _scopeName = scopeName;
+
+            if (createScopeFunction != null)
+            {
+                Log.Debug("Custom function to create the scope is provided, creating custom scope for type '{0}' with name '{1}'", typeof(T).GetSafeFullName(), _scopeName);
+
+                _scopeObject = createScopeFunction();
+            }
+            else
+            {
+                Log.Debug("No custom function to create the scope is provided, creating custom scope for type '{0}' with name '{1}' using TypeFactory", typeof(T).GetSafeFullName(), _scopeName);
+
+                var typeFactory = this.GetTypeFactory();
+                _scopeObject = typeFactory.CreateInstance<T>();
+            }
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the scope object.
+        /// </summary>
+        public T ScopeObject
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _scopeObject;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the current reference count for this
+        /// object.
+        /// </summary>
+        public int RefCount
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _refCount;
+                }
+            }
+        }
+        #endregion
+
+        #region IDisposable Members
+        /// <summary>
+        /// Dispose object, dereferencing or disposing the context it is managing.
+        /// </summary>
+        public void Dispose()
+        {
+            DeRef();
+        }
+        #endregion
+
+        #region Methods
+        private void AddRef()
+        {
+            lock (_lock)
+            {
+                _refCount += 1;
+
+                Log.Debug("Referencing type '{0}' with scope name '{1}', new ref count is {2}", typeof(T).GetSafeFullName(), _scopeName, _refCount);
+            }
+        }
+
+        private void DeRef()
+        {
+            lock (_lock)
+            {
+                _refCount -= 1;
+
+                Log.Debug("Dereferencing type '{0}' with scope name '{1}', new ref count is {2}", typeof(T).GetSafeFullName(), _scopeName, _refCount);
+
+                if (_refCount == 0)
+                {
+                    Log.Debug("Type '{0}' with scope name '{1}' has reached a ref count of 0, scope is closed now", typeof(T).GetSafeFullName(), _scopeName);
+
+                    var scopeObjectAsDisposable = _scopeObject as IDisposable;
+                    if (scopeObjectAsDisposable != null)
+                    {
+                        scopeObjectAsDisposable.Dispose();
+                    }
+
+                    _scopeObject = null;
+
+                    _instances.Remove(_scopeName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the ContextManager object for the specified database.
+        /// </summary>
+        /// <param name="scopeName">Name of the scope.</param>
+        /// <param name="createScopeFunction">The create scope function. Can be <c>null</c>.</param>
+        /// <returns>The <see cref="ScopeManager{T}" />.</returns>
+        /// <exception cref="ArgumentException">The <paramref name="scopeName"/> is <c>null</c>.</exception>
+        public static ScopeManager<T> GetScopeManager(string scopeName = "", Func<T> createScopeFunction = null)
+        {
+            Argument.IsNotNull("scopeName", scopeName);
+
+            lock (_lock)
+            {
+                ScopeManager<T> scopeManager;
+
+                if (_instances.ContainsKey(scopeName))
+                {
+                    Log.Debug("Returning existing scope for type '{0}' with name '{1}'", typeof(T).GetSafeFullName(), scopeName);
+
+                    scopeManager = (ScopeManager<T>)(_instances[scopeName]);
+                }
+                else
+                {
+                    Log.Debug("Creating new scope for type '{0}' with name '{1}'", typeof(T).GetSafeFullName(), scopeName);
+
+                    scopeManager = new ScopeManager<T>(scopeName, createScopeFunction);
+                    _instances[scopeName] = scopeManager;
+                }
+
+                scopeManager.AddRef();
+                return scopeManager;
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Catel.Test/Catel.Test.NET40/Scoping/ScopeManagerFacts.cs
@@ -0,0 +1,84 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ScopeManagerFacts.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Test.Scoping
+{
+    using Catel.Scoping;
+
+#if NETFX_CORE
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+    internal class ScopeManagerFacts
+    {
+        #region Nested type: ScopingTest
+        [TestClass]
+        public class ScopingTest
+        {
+            #region Methods
+            [TestMethod]
+            public void SingleLevelScoping()
+            {
+                ScopeManager<object> scopeManager = null;
+
+                using (scopeManager = ScopeManager<object>.GetScopeManager("object"))
+                {
+                    Assert.AreEqual(1, scopeManager.RefCount);
+                }
+
+                Assert.AreEqual(0, scopeManager.RefCount);
+            }
+
+            [TestMethod]
+            public void MultipleLevelScoping()
+            {
+                ScopeManager<object> scopeManager = null;
+
+                using (scopeManager = ScopeManager<object>.GetScopeManager("object"))
+                {
+                    Assert.AreEqual(1, scopeManager.RefCount);
+
+                    using (ScopeManager<object>.GetScopeManager("object"))
+                    {
+                        Assert.AreEqual(2, scopeManager.RefCount);
+
+                        using (ScopeManager<object>.GetScopeManager("object"))
+                        {
+                            Assert.AreEqual(3, scopeManager.RefCount);
+                        }
+
+                        Assert.AreEqual(2, scopeManager.RefCount);
+                    }
+
+                    Assert.AreEqual(1, scopeManager.RefCount);
+                }
+
+                Assert.AreEqual(0, scopeManager.RefCount);
+            }
+
+            [TestMethod]
+            public void CustomScopeCreationTest()
+            {
+                var obj1 = "15";
+                var obj2 = "16";
+
+                using (var scope1Manager = ScopeManager<string>.GetScopeManager(createScopeFunction: () => obj1))
+                {
+                    using (var scope2Manager = ScopeManager<string>.GetScopeManager(createScopeFunction: () => obj2))
+                    {
+                        Assert.AreEqual(obj1, scope2Manager.ScopeObject);
+                        Assert.AreEqual(2, scope2Manager.RefCount);
+                    }
+                }
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
