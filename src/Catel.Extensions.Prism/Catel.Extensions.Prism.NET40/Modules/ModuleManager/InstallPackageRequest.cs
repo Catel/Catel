bf36e75d1972afc89497ffb1f6524b900f01004f
alexfdezsauco <alexander.fernandez.sauco@gmail.com>
2013-10-07 08:30:44 -0400

    (+) Added support to install or distribute NuGet based modules trough NuGetBasedModuleCatalog

---
+++ src/Catel.Extensions.Prism/Catel.Extensions.Prism.NET40/Modules/ModuleManager/InstallPackageRequest.cs
@@ -0,0 +1,122 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InstallPackageRequest.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The install package request.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.Modules.ModuleManager
+{
+    using System.Diagnostics;
+    using System.IO;
+
+    using NuGet;
+
+    /// <summary>
+    /// The install package request.
+    /// </summary>
+    internal class InstallPackageRequest
+    {
+        #region Fields
+
+        /// <summary>
+        /// The module catalog.
+        /// </summary>
+        private readonly NuGetBasedModuleCatalog moduleCatalog;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InstallPackageRequest"/> class.
+        /// </summary>
+        /// <param name="packageRepository">
+        /// The package repository.
+        /// </param>
+        /// <param name="package">
+        /// The package.
+        /// </param>
+        /// <param name="moduleCatalog">
+        /// The module catalog.
+        /// </param>
+        public InstallPackageRequest(
+            IPackageRepository packageRepository, 
+            IPackage package, 
+            NuGetBasedModuleCatalog moduleCatalog)
+        {
+            this.PackageRepository = packageRepository;
+            this.Package = package;
+            this.moduleCatalog = moduleCatalog;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets a value indicating whether allow prerelease versions.
+        /// </summary>
+        public bool AllowPrereleaseVersions
+        {
+            get
+            {
+                return this.moduleCatalog.AllowPrereleaseVersions;
+            }
+        }
+
+        /// <summary>
+        /// Gets the framework name identifier.
+        /// </summary>
+        public string FrameworkNameIdentifier
+        {
+            get
+            {
+                return this.moduleCatalog.FrameworkNameIdentifier;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether ignore dependencies.
+        /// </summary>
+        public bool IgnoreDependencies
+        {
+            get
+            {
+                return this.moduleCatalog.IgnoreDependencies;
+            }
+        }
+
+        /// <summary>
+        /// Gets the output directory.
+        /// </summary>
+        public string OutputDirectory
+        {
+            get
+            {
+                string outputDirectory = this.moduleCatalog.OutputDirectory;
+                if (!Path.IsPathRooted(outputDirectory))
+                {
+                    string mainModuleDirectory = IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+                    outputDirectory = IO.Path.Combine(mainModuleDirectory, this.moduleCatalog.OutputDirectory);
+                }
+
+                return outputDirectory;
+            }
+        }
+
+        /// <summary>
+        /// Gets the package.
+        /// </summary>
+        public IPackage Package { get; private set; }
+
+        /// <summary>
+        /// Gets the package repository.
+        /// </summary>
+        public IPackageRepository PackageRepository { get; private set; }
+
+        #endregion
+    }
+}
---
+++ src/Catel.Extensions.Prism/Catel.Extensions.Prism.NET40/Modules/NuGetBasedModuleCatalog.cs
@@ -0,0 +1,57 @@
+﻿namespace Catel.Modules
+{
+    /// <summary>
+    /// The nuget based module catelog.
+    /// </summary>
+    public sealed class NuGetBasedModuleCatalog : ModuleCatalog
+    {
+        #region Constructors
+        /// <summary>
+        /// The nuget based module catalog.
+        /// </summary>
+        public NuGetBasedModuleCatalog()
+        {
+            this.PackageSource = "https://nuget.org/api/v2/";
+            this.OutputDirectory = "packages";
+            this.AllowPrereleaseVersions = false;
+            this.IgnoreDependencies = true;
+#if NET40
+            this.FrameworkNameIdentifier = "NET40";
+#elif NET45 
+            this.FrameworkNameIdentifier = "NET45";
+#elif SL4
+            this.FrameworkNameIdentifier = "SL4";
+#else
+            this.FrameworkNameIdentifier = "SL5";
+#endif
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the package source
+        /// </summary>
+        public string PackageSource { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the output directory
+        /// </summary>
+        public string OutputDirectory { get; set; }
+
+        // TODO: throw an exception if a value is not compatible with the target platform. 
+        /// <summary>
+        ///  Gets or sets the output directory
+        /// </summary>
+        /// <remarks>NuGet like framework name identifier string, for instance <c>NET35</c>, <c>NET40</c>, <c>NET45</c>, <c>SL4</c> and so on</remarks>
+        public string FrameworkNameIdentifier { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public bool IgnoreDependencies { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public bool AllowPrereleaseVersions { get; set; }
+        #endregion
+    }
+}
