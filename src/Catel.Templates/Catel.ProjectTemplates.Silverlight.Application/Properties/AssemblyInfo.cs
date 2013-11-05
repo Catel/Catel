870659fc7f841f7254ffa538fb590557e42bc402
Geert van Horrik <geert@catenalogic.com>
2013-11-05 20:16:15 +0100

    (*) CTL-217 Created on-line project templates for Catel

--- src/Catel.Templates/Catel.ProjectTemplates.Silverlight.Application/Properties/AssemblyInfo.cs
+++ src/Catel.Templates/Catel.ProjectTemplates.Silverlight.Application/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Catel.Templates")]
+[assembly: AssemblyTitle("Catel.ProjectTemplates.Silverlight.Application")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Catel.Templates")]
+[assembly: AssemblyProduct("Catel.ProjectTemplates.Silverlight.Application")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
