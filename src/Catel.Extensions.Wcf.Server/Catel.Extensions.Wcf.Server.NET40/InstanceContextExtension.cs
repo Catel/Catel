699f2444a6727a1e9c669279c88d5086aafb490c
Rajiv Précieux Mounguengue <rajiv.mounguengue@hotmail.fr>
2013-11-07 13:16:21 +0100

    (+) CTL-42 Added files for Catel.Extensions.Wcf.Server. Unit tests lefts.

---
+++ src/Catel.Extensions.Wcf.Server/Catel.Extensions.Wcf.Server.NET40/InstanceContextExtension.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InstanceContextExtension.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.ServiceModel
+{
+    using System;
+    using System.ServiceModel;
+    using IoC;
+
+    /// <summary>
+    /// </summary>
+    public class InstanceContextExtension : IExtension<InstanceContext>
+    {
+        #region Fields
+        /// <summary>
+        ///     The _child container
+        /// </summary>
+        private IServiceLocator _childContainer;
+        #endregion
+
+        #region IExtension<InstanceContext> Members
+        /// <summary>
+        ///     Attaches the specified owner.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        public void Attach(InstanceContext owner)
+        {
+        }
+
+        /// <summary>
+        ///     Detaches the specified owner.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        public void Detach(InstanceContext owner)
+        {
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        ///     Gets the child container.
+        /// </summary>
+        /// <param name="serviceLocator">The service locator.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="serviceLocator" /> is <c>null</c>.</exception>
+        public IServiceLocator GetChildContainer(IServiceLocator serviceLocator)
+        {
+            Argument.IsNotNull("serviceLocator", serviceLocator);
+
+            return _childContainer ?? (_childContainer = serviceLocator);
+        }
+
+        /// <summary>
+        ///     Disposes the of child container.
+        /// </summary>
+        public void DisposeChildContainer()
+        {
+            _childContainer = null;
+        }
+        #endregion
+    }
+}
---
+++ src/Catel.Extensions.Wcf.Server/Catel.Extensions.Wcf.Server.NET40/Serialization/DataType.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataType.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.ServiceModel.Serialization
+{
+    /// <summary>
+    /// The data types enum.
+    /// </summary>
+    public enum DataType
+    {
+        /// <summary>
+        /// The string
+        /// </summary>
+        String,
+
+        /// <summary>
+        /// The date time
+        /// </summary>
+        DateTime,
+
+        /// <summary>
+        /// The character
+        /// </summary>
+        Char,
+
+        /// <summary>
+        /// The int16
+        /// </summary>
+        Int16,
+
+        /// <summary>
+        /// The int32
+        /// </summary>
+        Int32,
+
+        /// <summary>
+        /// The int64
+        /// </summary>
+        Int64,
+
+        /// <summary>
+        /// The single
+        /// </summary>
+        Single,
+
+        /// <summary>
+        /// The double
+        /// </summary>
+        Double,
+
+        /// <summary>
+        /// The decimal
+        /// </summary>
+        Decimal,
+
+        /// <summary>
+        /// The bool
+        /// </summary>
+        Bool,
+
+        /// <summary>
+        /// The collection
+        /// </summary>
+        Collection,
+
+        /// <summary>
+        /// The object
+        /// </summary>
+        Object,
+
+        /// <summary>
+        /// The undefined
+        /// </summary>
+        Undefined
+    }
+}
