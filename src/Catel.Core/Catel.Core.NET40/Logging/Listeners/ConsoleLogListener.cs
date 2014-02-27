a14efbd29b3e3b54d8b25feb7ae44a8cc38be637
Geert van Horrik <geert@catenalogic.com>
2014-02-27 17:30:22 +0100

    (*) CTL-370 Add colors to ConsoleLogListener

--- src/Catel.Core/Catel.Core.NET40/Logging/Listeners/ConsoleLogListener.cs
+++ src/Catel.Core/Catel.Core.NET40/Logging/Listeners/ConsoleLogListener.cs
@@ -6,12 +6,37 @@
 namespace Catel.Logging
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Log listener that writes to the console.
     /// </summary>
     public class ConsoleLogListener : LogListenerBase
     {
+#if NET
+        private class ConsoleColorSet
+        {
+            public ConsoleColorSet(ConsoleColor background, ConsoleColor foreground)
+            {
+                Background = background;
+                Foreground = foreground;
+            }
+
+            public ConsoleColor Background { get; private set; }
+            public ConsoleColor Foreground { get; private set; }
+        }
+
+        private static readonly Dictionary<LogEvent, ConsoleColorSet> ColorSets = new Dictionary<LogEvent, ConsoleColorSet>();
+
+        static ConsoleLogListener()
+        {
+            ColorSets[LogEvent.Debug] = new ConsoleColorSet(ConsoleColor.Black, ConsoleColor.Gray);
+            ColorSets[LogEvent.Info] = new ConsoleColorSet(ConsoleColor.Black, ConsoleColor.White);
+            ColorSets[LogEvent.Warning] = new ConsoleColorSet(ConsoleColor.Black, ConsoleColor.Yellow);
+            ColorSets[LogEvent.Error] = new ConsoleColorSet(ConsoleColor.Black, ConsoleColor.Red);
+        }
+#endif
+
         /// <summary>
         /// Called when any message is written to the log.
         /// </summary>
@@ -23,11 +48,26 @@ namespace Catel.Logging
         {
             string consoleMessage = FormatLogEvent(log, message, logEvent, extraData);
 
+#if NET
+
+
+#endif
+
 #if NETFX_CORE || PCL
             System.Diagnostics.Debug.WriteLine(consoleMessage);
 #else
             Console.WriteLine(consoleMessage);
 #endif
         }
+
+#if NET
+        private static void UpdateConsoleColors(LogEvent logEvent)
+        {
+            var colorSet = ColorSets[logEvent];
+
+            Console.BackgroundColor = colorSet.Background;
+            Console.ForegroundColor = colorSet.Foreground;
+        }
+#endif
     }
 }
