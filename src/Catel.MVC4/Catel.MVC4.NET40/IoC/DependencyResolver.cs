b3df35ff6a2afb4fb8ae5fcda1c60ba6b4787fd4
Geert van Horrik <geert@catenalogic.com>
2014-03-15 22:14:22 +0100

    (+) CTL-389 Added support for MVC 5 (5.1.1) and updated MVC 4 to 4.0.30506

---
+++ src/Catel.MVC4/Catel.MVC4.NET40/IoC/DependencyResolver.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DependencyResolver.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.IoC
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Dependency resolver.
+    /// </summary>
+    public class DependencyResolver : System.Web.Mvc.IDependencyResolver
+    {
+        private readonly IServiceLocator _serviceLocator;
+        private readonly ITypeFactory _typeFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyResolver"/> class.
+        /// </summary>
+        /// <param name="serviceLocator">The service locator. If <c>null</c>, the <see cref="ServiceLocator.Default"/> will be used.</param>
+        public DependencyResolver(IServiceLocator serviceLocator = null)
+        {
+            if (serviceLocator == null)
+            {
+                serviceLocator = ServiceLocator.Default;
+            }
+
+            _serviceLocator = serviceLocator;
+            _typeFactory = serviceLocator.ResolveType<ITypeFactory>();
+        }
+
+        /// <summary>
+        /// Resolves singly registered services that support arbitrary object creation.
+        /// </summary>
+        /// <param name="serviceType">The type of the requested service or object.</param>
+        /// <returns>The requested service or object.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="serviceType" /> is <c>null</c>.</exception>
+        public object GetService(Type serviceType)
+        {
+            Argument.IsNotNull("serviceType", serviceType);
+
+            if (_serviceLocator.IsTypeRegistered(serviceType))
+            {
+                return _serviceLocator.ResolveType(serviceType);
+            }
+
+            if (!serviceType.IsInterface && !serviceType.IsAbstract)
+            {
+                return _typeFactory.CreateInstance(serviceType);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Resolves multiply registered services.
+        /// </summary>
+        /// <param name="serviceType">The type of the requested services.</param>
+        /// <returns>The requested services.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="serviceType" /> is <c>null</c>.</exception>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            Argument.IsNotNull("serviceType", serviceType);
+
+            if (_serviceLocator.IsTypeRegistered(serviceType))
+            {
+                return _serviceLocator.ResolveTypes(serviceType);
+            }
+
+            return new object[] { };
+        }
+    }
+}
