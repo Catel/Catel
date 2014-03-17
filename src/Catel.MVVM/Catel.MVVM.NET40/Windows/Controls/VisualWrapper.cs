a8c5a4167b2f350df0e7aab3e69bb786cbfbd860
Geert van Horrik <geert@catenalogic.com>
2014-03-17 11:15:07 +0100

    (+) Added MediaElementThreadFactory so it is easier to create media elements on separate threads in WPF

--- src/Catel.MVVM/Catel.MVVM.NET40/Windows/Controls/VisualWrapper.cs
+++ src/Catel.MVVM/Catel.MVVM.NET40/Windows/Controls/VisualWrapper.cs
@@ -21,71 +21,71 @@ namespace Catel.Windows.Controls
 #endif
 
     /// <summary>
-	/// This visual wrapper is used by VisualTargetPresentationSource
-	/// </summary>
+    /// This visual wrapper is used by VisualTargetPresentationSource
+    /// </summary>
 #if NETFX_CORE
 	[ContentProperty(Name = "Child")]
 #else
     [ContentProperty("Child")]
 #endif
     public class VisualWrapper : FrameworkElement
-	{
-		private Visual _child;
+    {
+        private Visual _child;
 
-		/// <summary>w
-		/// Gets or sets the child.
-		/// </summary>
-		/// <value>The child.</value>
-		public Visual Child
-		{
-			get
-			{
-			    return _child;
-			}
+        /// <summary>w
+        /// Gets or sets the child.
+        /// </summary>
+        /// <value>The child.</value>
+        public Visual Child
+        {
+            get
+            {
+                return _child;
+            }
 
-			set
-			{
-				if (_child != null)
-				{
-					RemoveVisualChild(_child);
-				}
+            set
+            {
+                if (_child != null)
+                {
+                    RemoveVisualChild(_child);
+                }
 
-				_child = value;
+                _child = value;
 
-				if (_child != null)
-				{
-					AddVisualChild(_child);
-				}
-			}
-		}
+                if (_child != null)
+                {
+                    AddVisualChild(_child);
+                }
+            }
+        }
 
-		/// <summary>
-		/// Overrides <see cref="M:System.Windows.Media.Visual.GetVisualChild(System.Int32)"/>, and returns a child at the specified index from a collection of child elements.
-		/// </summary>
-		/// <param name="index">The zero-based index of the requested child element in the collection.</param>
-		/// <returns>
-		/// The requested child element. This should not return null; if the provided index is out of range, an exception is thrown.
-		/// </returns>
-		protected override Visual GetVisualChild(int index)
-		{
-			if (_child != null && index == 0)
-			{
-				return _child;
-			}
-			
-			throw new ArgumentOutOfRangeException("index");
-		}
+        /// <summary>
+        /// Overrides <see cref="M:System.Windows.Media.Visual.GetVisualChild(System.Int32)"/>, and returns a child at the specified index from a collection of child elements.
+        /// </summary>
+        /// <param name="index">The zero-based index of the requested child element in the collection.</param>
+        /// <returns>
+        /// The requested child element. This should not return null; if the provided index is out of range, an exception is thrown.
+        /// </returns>
+        protected override Visual GetVisualChild(int index)
+        {
+            if (_child != null && index == 0)
+            {
+                return _child;
+            }
 
-		/// <summary>
-		/// Gets the number of visual child elements within this element.
-		/// </summary>
-		/// <value></value>
-		/// <returns>
-		/// The number of visual child elements for this element.
-		/// </returns>
-		protected override int VisualChildrenCount
-		{
-			get { return _child != null ? 1 : 0; }
-		}
-	}
+            throw new ArgumentOutOfRangeException("index");
+        }
+
+        /// <summary>
+        /// Gets the number of visual child elements within this element.
+        /// </summary>
+        /// <value></value>
+        /// <returns>
+        /// The number of visual child elements for this element.
+        /// </returns>
+        protected override int VisualChildrenCount
+        {
+            get { return _child != null ? 1 : 0; }
+        }
+    }
 }
--- src/Catel.MVVM/Catel.MVVM.NET45/Windows/Windows/PleaseWaitWindow.xaml.cs
+++ src/Catel.MVVM/Catel.MVVM.NET45/Windows/Windows/PleaseWaitWindow.xaml.cs
@@ -45,9 +45,7 @@ namespace Catel.Windows
     public partial class PleaseWaitWindow
     {
         #region Fields
-        private static readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
-
-        private Thread _thread;
+        private MediaElementThreadInfo _mediaElementThreadInfo;
 
         private readonly List<FrameworkElement> _dimmedElements = new List<FrameworkElement>();
         #endregion
@@ -345,10 +343,8 @@ namespace Catel.Windows
         /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            var hostVisuals = CreateMediaElementsOnWorkerThread();
-
-            indeterminateVisualWrapper.Child = hostVisuals[0];
-            //determinateVisualWrapper.Child = hostVisuals[1];
+            _mediaElementThreadInfo = MediaElementThreadFactory.CreateMediaElementsOnWorkerThread(CreateIndeterminateElement);
+            indeterminateVisualWrapper.Child = _mediaElementThreadInfo.HostVisual;
         }
 
         /// <summary>
@@ -373,10 +369,10 @@ namespace Catel.Windows
             Loaded -= OnLoaded;
             LayoutUpdated -= OnLayoutUpdated;
 
-            if (_thread != null)
+            if (_mediaElementThreadInfo != null)
             {
-                _thread.Abort();
-                _thread = null;
+                _mediaElementThreadInfo.Dispose();
+                _mediaElementThreadInfo = null;
             }
 
             // Make sure we are not leaving the owner window dimmed in case a direct call to Close is invoked
@@ -398,59 +394,9 @@ namespace Catel.Windows
             base.OnClosed(e);
         }
 
-        /// <summary>
-        /// Creates the media element on worker thread.
-        /// </summary>
-        /// <returns></returns>
-        private HostVisual[] CreateMediaElementsOnWorkerThread()
-        {
-            var visuals = new[] { new HostVisual(), new HostVisual() };
-
-            _thread = new Thread(WorkerThread);
-
-            _thread.SetApartmentState(ApartmentState.STA);
-            _thread.IsBackground = true;
-            _thread.Start(visuals);
-
-            _autoResetEvent.WaitOne();
-
-            return visuals;
-        }
-
-        private static void WorkerThread(object arg)
-        {
-            try
-            {
-                var hostVisuals = (HostVisual[])arg;
-
-                var indeterminateSource = new VisualTargetPresentationSource(hostVisuals[0]);
-                var determinateSource = new VisualTargetPresentationSource(hostVisuals[1]);
-
-                _autoResetEvent.Set();
-
-                indeterminateSource.RootVisual = CreateIndeterminateElement();
-                determinateSource.RootVisual = CreateDeterminateElement();
-
-                Dispatcher.Run();
-            }
-            catch
-            {
-            }
-        }
-
         private static FrameworkElement CreateIndeterminateElement()
         {
             return new LoaderAnimation { Width = 32, Height = 32 };
-        }
-
-        private static FrameworkElement CreateDeterminateElement()
-        {
-            return null;
-
-            //var progressBar = new ProgressBar { Minimum = 0, Maximum = 100 };
-            //progressBar.Value = 50;
-
-            //return progressBar;
         }
         #endregion
     }
