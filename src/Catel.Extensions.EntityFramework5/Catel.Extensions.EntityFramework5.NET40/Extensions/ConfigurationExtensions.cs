e83b3f500f90f9a055be2ef3114f3e32e20bbfbf
Geert van Horrik <geert@catenalogic.com>
2014-08-20 12:04:59 +0200

    (+) Added ConfigurationExtensions to Catel.Extensions.EntityFramework to ignore default Catel properties to make it easier to use ModelBase as base class for code-first entities

---
+++ src/Catel.Extensions.EntityFramework5/Catel.Extensions.EntityFramework5.NET40/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigurationExtensions.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Data
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    /// <summary>
+    /// Class EntityTypeConfigurationExtensions.
+    /// </summary>
+    public static class EntityTypeConfigurationExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Ignores the catel properties for database mappings.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns>EntityTypeConfiguration&lt;TEntity&gt;.</returns>
+        public static EntityTypeConfiguration<TEntity> IgnoreCatelProperties<TEntity>(this EntityTypeConfiguration<TEntity> configuration)
+            where TEntity : ModelBase
+        {
+            configuration.Ignore(x => x.BusinessRuleErrorCount);
+            configuration.Ignore(x => x.BusinessRuleWarningCount);
+            configuration.Ignore(x => x.FieldErrorCount);
+            configuration.Ignore(x => x.FieldWarningCount);
+            configuration.Ignore(x => x.HasErrors);
+            configuration.Ignore(x => x.HasWarnings);
+            configuration.Ignore(x => x.IsDirty);
+            configuration.Ignore(x => x.IsEditable);
+            configuration.Ignore(x => x.IsInEditSession);
+            configuration.Ignore(x => x.IsReadOnly);
+            configuration.Ignore(x => x.Mode);
+            configuration.Ignore(x => x.ValidationContext);
+            configuration.Ignore(x => x.Validator);
+
+            return configuration;
+        }
+        #endregion
+    }
+}
