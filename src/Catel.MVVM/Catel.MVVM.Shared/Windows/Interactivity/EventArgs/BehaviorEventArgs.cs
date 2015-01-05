5234dcf4f66188ab1cc5c35089375a6bccb139fd
Geert van Horrik <geert@catenalogic.com>
2015-01-05 13:49:57 +0100

    It's 2015

--- src/Catel.MVVM/Catel.MVVM.Shared/Windows/Interactivity/EventArgs/BehaviorEventArgs.cs
+++ src/Catel.MVVM/Catel.MVVM.Shared/Windows/Interactivity/EventArgs/BehaviorEventArgs.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="BehaviorEventArgs.cs" company="Catel development team">
-//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
--- src/Catel.Test/Catel.Test.Shared/Data/Exceptions/InvalidPropertyValueExceptionFacts.cs
+++ src/Catel.Test/Catel.Test.Shared/Data/Exceptions/InvalidPropertyValueExceptionFacts.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="InvalidPropertyValueExceptionTest.cs" company="Catel development team">
-//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
