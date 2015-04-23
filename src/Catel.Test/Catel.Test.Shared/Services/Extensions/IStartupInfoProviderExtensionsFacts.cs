8ed38657ed57cdfbf9236c0a7f4ecf9307c84971
Geert van Horrik <geert@catenalogic.com>
2015-04-23 12:49:03 +0200

    (+) CTL-631 Create IStartupInfoProvider extension method to create a command line

---
+++ src/Catel.Test/Catel.Test.Shared/Services/Extensions/IStartupInfoProviderExtensionsFacts.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IStartupInfoProviderExtensionsFacts.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Test.Services
+{
+    using Catel.Services;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IStartupInfoProviderExtensionsFacts
+    {
+        [TestCase(new[] { "-a", "testa", "-b", "testb" }, "-a testa -b testb")]
+        [TestCase(new[] { "-a", "testa", "-b", "test with spaces" }, "-a testa -b \"test with spaces\"")]
+        public void TheGetCommandLineMethod(string[] input, string expectedCommandLine)
+        {
+            var fixture = new StartUpInfoProviderFixture(input);
+
+            var commandLine = fixture.GetCommandLine();
+
+            Assert.AreEqual(expectedCommandLine, commandLine);
+        }
+    }
+}
