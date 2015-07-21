51fd7c45b8aac1b9af7fa16361583d6a78531f2f
Geert van Horrik <geert@catenalogic.com>
2015-07-21 14:31:39 +0200

    (+) CTL-615 Added support for .NET 4.6

---
+++ src/Catel.Extensions.Controls/Catel.Extensions.Controls.NET46/Windows/Controls/MultiLineInput.xaml.cs
@@ -0,0 +1,125 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MultiLineInput.xaml.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.Windows.Controls
+{
+    using System.Windows;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for MultiLineInput.xaml
+    /// </summary>
+    public partial class MultiLineInput : System.Windows.Controls.UserControl
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of this control.
+        /// </summary>
+        public MultiLineInput()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets Caption.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper for the Caption dependency property.
+        /// </remarks>
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty definition as the backing store for Caption.
+        /// </summary>
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof(string), typeof(MultiLineInput), new UIPropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Gets or sets Text.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper for the Text dependency property.
+        /// </remarks>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty definition as the backing store for Text.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MultiLineInput), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets MaxTextLength.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper for the MaxTextLength dependency property.
+        /// </remarks>
+        public int MaxTextLength
+        {
+            get { return (int)GetValue(MaxTextLengthProperty); }
+            set { SetValue(MaxTextLengthProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty definition as the backing store for MaxTextLength.
+        /// </summary>
+        public static readonly DependencyProperty MaxTextLengthProperty =
+            DependencyProperty.Register("MaxTextLength", typeof(int), typeof(MultiLineInput), new UIPropertyMetadata(0));
+
+        /// <summary>
+        /// Gets or sets IsReadOnly.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper for the IsReadOnly dependency property.
+        /// </remarks>
+        public bool IsReadOnly
+        {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty definition as the backing store for IsReadOnly.
+        /// </summary>
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(MultiLineInput), new UIPropertyMetadata(false));
+
+        /// <summary>
+        /// Gets or sets TextBoxBackground.
+        /// </summary>
+        /// <remarks>
+        /// Wrapper for the TextBoxBackground dependency property.
+        /// </remarks>
+        public Brush TextBoxBackground
+        {
+            get { return (Brush)GetValue(TextBoxBackgroundProperty); }
+            set { SetValue(TextBoxBackgroundProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty definition as the backing store for TextBoxBackground.
+        /// </summary>
+        public static readonly DependencyProperty TextBoxBackgroundProperty =
+            DependencyProperty.Register("TextBoxBackground", typeof(Brush), typeof(MultiLineInput), new UIPropertyMetadata(Brushes.White));
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ src/Catel.Extensions.Controls/Catel.Extensions.Controls.NET46/Windows/Controls/TraceOutputControl/Views/TraceOutputControl.xaml.cs
@@ -0,0 +1,98 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TraceOutputControl.xaml.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Windows.Controls
+{
+    using System.Linq;
+    using System.Windows;
+    using Logging;
+    using MVVM.Views;
+
+    /// <summary>
+    /// Interaction logic for TraceOutputControl.xaml
+    /// </summary>
+    [ObsoleteEx(ReplacementTypeOrMember = "Orc.Controls, see https://github.com/wildgums/orc.controls", TreatAsErrorFromVersion = "4.2", RemoveInVersion = "5.0")]
+    public partial class TraceOutputControl : UserControl
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes static members of the <see cref="TraceOutputControl"/> class.
+        /// </summary>
+        /// <remarks>This method is required for design time support.</remarks>
+        static TraceOutputControl()
+        {
+            typeof(TraceOutputControl).AutoDetectViewPropertiesToSubscribe();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TraceOutputControl"/> class.
+        /// </summary>
+        public TraceOutputControl()
+        {
+            InitializeComponent();
+
+            logListView.SelectionChanged += (sender, args) =>
+            {
+                var vm = ViewModel as TraceOutputViewModel;
+                if (vm != null)
+                {
+                    vm.SelectedTraceEntries = logListView.SelectedItems.Cast<TraceEntry>().ToList();
+                }
+            };
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets whether the Catel logging should be ignored.
+        /// </summary>
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewModelWins)]
+        public bool IgnoreCatelLogging
+        {
+            get { return (bool)GetValue(IgnoreCatelLoggingProperty); }
+            set { SetValue(IgnoreCatelLoggingProperty, value); }
+        }
+
+        /// <summary>
+        /// Using a DependencyProperty as the backing store for IgnoreCatelLogging.  This enables animation, styling, binding, etc...
+        /// </summary>
+        public static readonly DependencyProperty IgnoreCatelLoggingProperty =
+            DependencyProperty.Register("IgnoreCatelLogging", typeof(bool), typeof(TraceOutputControl), new PropertyMetadata(true));
+
+        /// <summary>
+        /// Gets or sets the selected level.
+        /// </summary>
+        /// <value>The selected level.</value>
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewModelWins)]
+        public LogEvent SelectedLevel
+        {
+            get { return (LogEvent)GetValue(SelectedLevelProperty); }
+            set { SetValue(SelectedLevelProperty, value); }
+        }
+
+        /// <summary>
+        /// Using a DependencyProperty as the backing store for SelectedLevel.  This enables animation, styling, binding, etc...
+        /// </summary>
+        public static readonly DependencyProperty SelectedLevelProperty =
+            DependencyProperty.Register("SelectedLevel", typeof(LogEvent), typeof(TraceOutputControl), new PropertyMetadata(LogEvent.Debug));
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Clears all messages from the control.
+        /// </summary>
+        public void Clear()
+        {
+            var vm = ViewModel as TraceOutputViewModel;
+            if (vm != null)
+            {
+                vm.ClearOutput.Execute();
+            }
+        }
+        #endregion
+    }
+}
