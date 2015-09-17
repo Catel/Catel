43b394a589076b8aa07c5975b4dde7a8ffd7322c
Geert van Horrik <geert@catenalogic.com>
2015-09-17 17:34:42 +0200

    (+) CTL-732 Add DeviceHelper so it's easy to determine on what kind of device the code is being executed

---
+++ src/Catel.Core/Catel.Core.Shared/Devices/DeviceType.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DeviceFamily.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel
+{
+    /// <summary>
+    /// Known device types.
+    /// </summary>
+    public enum DeviceType
+    {
+        /// <summary>
+        /// A windows phone.
+        /// </summary>
+        Phone,
+
+        /// <summary>
+        /// Tablet.
+        /// </summary>
+        Tablet,
+
+        /// <summary>
+        /// Desktop.
+        /// </summary>
+        Desktop,
+
+        /// <summary>
+        /// An xbox.
+        /// </summary>
+        Xbox,
+
+        /// <summary>
+        /// An Internet of Things (IoT) device.
+        /// </summary>
+        IoT,
+
+        /// <summary>
+        /// An unknown device.
+        /// </summary>
+        Unknown
+    }
+}
