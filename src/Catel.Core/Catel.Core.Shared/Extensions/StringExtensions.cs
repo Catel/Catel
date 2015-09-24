98caba7b4e50ec4ab6a1cbf2b4b5b8905a36c131
Geert van Horrik <geert@catenalogic.com>
2015-09-24 14:44:16 +0200

    Added string comparison extension methods

--- src/Catel.Core/Catel.Core.Shared/Extensions/StringExtensions.cs
+++ src/Catel.Core/Catel.Core.Shared/Extensions/StringExtensions.cs
@@ -7,12 +7,73 @@
 
 namespace Catel
 {
+    using System;
+
     /// <summary>
     /// The string extensions.
     /// </summary>
     public static partial class StringExtensions
     {
         #region Methods
+        /// <summary>
+        /// Executes a string comparison that is case insensitive.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="valueToCheck">The value to check.</param>
+        /// <returns><c>true</c> if the strings are equal, <c>false</c> otherwise.</returns>
+        public static bool EqualsIgnoreCase(this string str, string valueToCheck)
+        {
+            return string.Equals(str, valueToCheck, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Determines whether the specified string contains the value to check.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="valueToCheck">The value to check.</param>
+        /// <returns><c>true</c> if the string contains the value to check, <c>false</c> otherwise.</returns>
+        public static bool ContainsIgnoreCase(this string str, string valueToCheck)
+        {
+            var index = IndexOfIgnoreCase(str, valueToCheck);
+            return index >= 0;
+        }
+
+        /// <summary>
+        /// Determines whether the string starts with the value to check.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="valueToCheck">The value to check.</param>
+        /// <returns><c>true</c> if the string starts with the value to check, <c>false</c> otherwise.</returns>
+        public static bool StartsWithIgnoreCase(this string str, string valueToCheck)
+        {
+            var startsWith = str.StartsWith(valueToCheck, StringComparison.OrdinalIgnoreCase);
+            return startsWith;
+        }
+
+        /// <summary>
+        /// Determines whether the string ends with the value to check.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="valueToCheck">The value to check.</param>
+        /// <returns><c>true</c> if the string ends with the value to check, <c>false</c> otherwise.</returns>
+        public static bool EndsWithIgnoreCase(this string str, string valueToCheck)
+        {
+            var endsWith = str.EndsWith(valueToCheck, StringComparison.OrdinalIgnoreCase);
+            return endsWith;
+        }
+
+        /// <summary>
+        /// Determines the index of the value to check inside the specified string.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="valueToCheck">The value to check.</param>
+        /// <returns>The index or <c>-1</c> if not found.</returns>
+        public static int IndexOfIgnoreCase(this string str, string valueToCheck)
+        {
+            var index = str.IndexOf(valueToCheck, StringComparison.OrdinalIgnoreCase);
+            return index;
+        }
+
         /// <summary>
         /// Prepares a string value as search filter by trimming it and making it lower-case.
         /// </summary>
