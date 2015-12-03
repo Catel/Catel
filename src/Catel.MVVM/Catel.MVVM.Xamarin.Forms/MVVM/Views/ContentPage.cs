daed6ad553c8c1fe090cbe189d62f20963181ee4
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2015-12-03 13:23:46 -0500

    Added basic implementation for ContentPage

--- src/Catel.MVVM/Catel.MVVM.Xamarin.Forms/MVVM/Views/ContentPage.cs
+++ src/Catel.MVVM/Catel.MVVM.Xamarin.Forms/MVVM/Views/ContentPage.cs
@@ -5,21 +5,29 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-namespace Catel.MVVM.Views  
-{
-    using System;
-    using MVVM;
+using System;
 
+namespace Catel.MVVM.Views
+{
     /// <summary>
-    /// 
     /// </summary>
-    public class ContentPage : global::Xamarin.Forms.ContentPage, IView
+    public class ContentPage : Xamarin.Forms.ContentPage, IView
     {
-        public IViewModel ViewModel { get; }
+        protected ContentPage()
+        {
+            BindingContextChanged += OnBindingContextChanged;
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        public IViewModel ViewModel => DataContext as IViewModel;
 
         public event EventHandler<EventArgs> ViewModelChanged;
 
-        public object DataContext { get; set; }
+        public object DataContext
+        {
+            get { return BindingContext; }
+            set { BindingContext = value; }
+        }
 
         public object Tag { get; set; }
 
@@ -28,5 +36,15 @@ namespace Catel.MVVM.Views
         public event EventHandler<EventArgs> Unloaded;
 
         public event EventHandler<DataContextChangedEventArgs> DataContextChanged;
+
+        private void OnDataContextChanged(object sender, DataContextChangedEventArgs dataContextChangedEventArgs)
+        {
+            ViewModelChanged.SafeInvoke(this);
+        }
+
+        private void OnBindingContextChanged(object sender, EventArgs eventArgs)
+        {
+            DataContextChanged.SafeInvoke(this, new DataContextChangedEventArgs(null, BindingContext));
+        }
     }
 }
