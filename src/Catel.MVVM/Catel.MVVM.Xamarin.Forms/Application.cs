61209818883a31fdf39ef3fe26adbfdbfc9196be
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2015-12-03 16:54:40 -0500

    Added Application<TApplicationPageViewModel> to simplify the initialization

---
+++ src/Catel.MVVM/Catel.MVVM.Xamarin.Forms/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Catel.IoC;
+using Catel.MVVM;
+using Xamarin.Forms;
+
+namespace Catel
+{
+    public class Application<TViewModel> : Application where TViewModel : IViewModel
+    {
+        protected Application()
+        {
+            // TODO: ModuleInit should work instead this.
+            Core.ModuleInitializer.Initialize();
+            ModuleInitializer.Initialize();
+
+            // TODO: Improve this approach.
+            var viewLocator = this.GetDependencyResolver().Resolve<IViewLocator>();
+            var viewModelFactory = this.GetDependencyResolver().Resolve<IViewModelFactory>();
+            var resolveView = viewLocator.ResolveView(typeof(TViewModel));
+            ApplicationPage = (Page)Activator.CreateInstance(resolveView);
+            ApplicationPage.BindingContext = viewModelFactory.CreateViewModel<TViewModel>(null);
+        }
+
+        protected Page ApplicationPage { get; private set; }
+    }
+}
