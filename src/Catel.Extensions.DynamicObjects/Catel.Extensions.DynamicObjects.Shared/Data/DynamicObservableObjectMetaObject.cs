a7bc019094b2648558a379f11eaeb8175f4a962b
michu <michalp@op.pl>
2016-01-06 17:44:01 +0100

    Add DynamicObservableObject implementation + tests

---
+++ src/Catel.Extensions.DynamicObjects/Catel.Extensions.DynamicObjects.Shared/Data/DynamicObservableObjectMetaObject.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DynamicObservableObjectMetaObject.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2016 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.Data
+{
+    using Reflection;
+    using System.Dynamic;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    /// <summary>
+    /// Class containing metadata for the <see cref="DynamicObservableObject"/>.
+    /// </summary>
+    public class DynamicObservableObjectMetaObject : DynamicMetaObject
+    {
+        private static readonly MethodInfo _getValueMethodInfo;
+        private static readonly MethodInfo _setValueMethodInfo;
+
+        /// <summary>
+        /// Initializes static members of the <see cref=" DynamicObservableObjectMetaObject"/> class.
+        /// </summary>
+        static DynamicObservableObjectMetaObject()
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Public;
+
+            _getValueMethodInfo = typeof(DynamicObservableObject).GetMethodEx("GetValue", bindingFlags).MakeGenericMethod(new[] { typeof(object) });
+            _setValueMethodInfo = typeof(DynamicObservableObject).GetMethodEx("SetValue", bindingFlags);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicObservableObjectMetaObject"/> class.
+        /// </summary>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="observableObject">The observable object.</param>
+        public DynamicObservableObjectMetaObject(Expression parameter, DynamicObservableObject observableObject)
+            : base(parameter, BindingRestrictions.Empty, observableObject)
+        {
+
+        }
+
+        /// <summary>
+        /// Performs the binding of the dynamic set member operation.
+        /// </summary>
+        /// <param name="binder">An instance of the <see cref="T:System.Dynamic.SetMemberBinder" /> that represents the details of the dynamic operation.</param>
+        /// <param name="value">The <see cref="T:System.Dynamic.DynamicMetaObject" /> representing the value for the set member operation.</param>
+        /// <returns>The new <see cref="T:System.Dynamic.DynamicMetaObject" /> representing the result of the binding.</returns>
+        public override DynamicMetaObject BindSetMember(SetMemberBinder binder, DynamicMetaObject value)
+        {
+            var propertyName = binder.Name;
+            var propertyType = binder.ReturnType;
+
+            var valueExpression = Expression.Convert(value.Expression, typeof(object));
+            var parameters = new Expression[]
+            {
+                Expression.Constant(propertyName),
+                valueExpression
+            };
+
+            var self = Expression.Convert(Expression, LimitType);
+            var methodCall = Expression.Call(self, _setValueMethodInfo, parameters);
+
+            // Note: required because we must return a value as well
+            var finalExpression = Expression.Block(methodCall, valueExpression);
+            var restrictions = BindingRestrictions.GetTypeRestriction(Expression, LimitType);
+
+            var setValue = new DynamicMetaObject(finalExpression, restrictions);
+            return setValue;
+        }
+
+        /// <summary>
+        /// Performs the binding of the dynamic get member operation.
+        /// </summary>
+        /// <param name="binder">An instance of the <see cref="T:System.Dynamic.GetMemberBinder" /> that represents the details of the dynamic operation.</param>
+        /// <returns>The new <see cref="T:System.Dynamic.DynamicMetaObject" /> representing the result of the binding.</returns>
+        public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
+        {
+            var propertyName = binder.Name;
+            var propertyType = binder.ReturnType;
+
+            var parameters = new Expression[]
+            {
+                Expression.Constant(propertyName)
+            };
+
+            var self = Expression.Convert(Expression, LimitType);
+            var methodCall = Expression.Call(self, _getValueMethodInfo, parameters);
+            var restrictions = BindingRestrictions.GetTypeRestriction(Expression, LimitType);
+
+            var getValue = new DynamicMetaObject(methodCall, restrictions);
+            return getValue;
+        }
+    }
+}
---
+++ src/Catel.Test/Catel.Test.Shared/Extensions/DynamicObjects/DynamicObservableObjectFacts.cs
@@ -0,0 +1,223 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DynamicModelBaseFacts.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2016 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Test.Extensions.DynamicObjects
+{
+    using System;
+    using System.IO;
+    using Catel.Data;
+    using Catel.Runtime.Serialization;
+
+    using NUnit.Framework;
+
+    public class DynamicObservableObjectFacts
+    {
+        public class CustomObject : DynamicObservableObject
+        {
+        }
+
+        [TestFixture]
+        public class TheGetValueAndSetValueProperties
+        {
+            [TestCase]
+            public void CorrectlyReturnsTheRightValue_WhenSetViaDynamicProperty()
+            {
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Settings value via dynamic property, getting via dynamic property and via GetValue<T> method.
+                DateTime dt = DateTime.ParseExact("2016-01-01 01:01:01", "yyyy-MM-dd HH:mm:ss", null);
+                dynamicObservableObject.Property1 = "test";
+                dynamicObservableObject.Property2 = 100;
+                dynamicObservableObject.Property3 = 3.14F;
+                dynamicObservableObject.Property4 = 1.2M;
+                dynamicObservableObject.Property5 = dt;
+
+                Assert.AreEqual("test", dynamicObservableObject.Property1);
+                Assert.AreEqual(100, dynamicObservableObject.Property2);
+                Assert.AreEqual(3.14F, dynamicObservableObject.Property3);
+                Assert.AreEqual(1.2M, dynamicObservableObject.Property4);
+                Assert.AreEqual(dt, dynamicObservableObject.Property5);
+
+                Assert.AreEqual("test", observableObject.GetValue<string>("Property1"));
+                Assert.AreEqual(100, observableObject.GetValue<int>("Property2"));
+                Assert.AreEqual(3.14F, observableObject.GetValue<float>("Property3"));
+                Assert.AreEqual(1.2M, observableObject.GetValue<decimal>("Property4"));
+                Assert.AreEqual(dt, observableObject.GetValue<DateTime>("Property5"));
+            }
+
+            [TestCase]
+            public void CorrectlyReturnsTheRightValue_WhenSetViaSetValueMethod()
+            {
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Setting value via SetValue method, getting via dynamic property and via GetValue<T> method.
+                DateTime dt = DateTime.ParseExact("2016-01-01 01:01:01", "yyyy-MM-dd HH:mm:ss", null);
+                observableObject.SetValue("Property1", "test");
+                observableObject.SetValue("Property2", 100);
+                observableObject.SetValue("Property3", 3.14F);
+                observableObject.SetValue("Property4", 1.2M);
+                observableObject.SetValue("Property5", dt);
+
+                Assert.AreEqual("test", dynamicObservableObject.Property1);
+                Assert.AreEqual(100, dynamicObservableObject.Property2);
+                Assert.AreEqual(3.14F, dynamicObservableObject.Property3);
+                Assert.AreEqual(1.2M, dynamicObservableObject.Property4);
+                Assert.AreEqual(dt, dynamicObservableObject.Property5);
+
+                Assert.AreEqual("test", observableObject.GetValue<string>("Property1"));
+                Assert.AreEqual(100, observableObject.GetValue<int>("Property2"));
+                Assert.AreEqual(3.14F, observableObject.GetValue<float>("Property3"));
+                Assert.AreEqual(1.2M, observableObject.GetValue<decimal>("Property4"));
+                Assert.AreEqual(dt, observableObject.GetValue<DateTime>("Property5"));
+            }
+
+            [TestCase]
+            public void RaisesAdvancedPropertyChangingEvents_WhenSetViaDynamicProperty()
+            {
+                var counter = 0;
+                var propertyName = default(string);
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Setting value via dynamic property.
+                dynamicObservableObject.Property1 = "oldtest";
+                observableObject.PropertyChanging += (sender, e) =>
+                {
+                    AdvancedPropertyChangingEventArgs args = e as AdvancedPropertyChangingEventArgs;
+                    if (args != null)
+                    {
+                        counter++;
+                        propertyName = args.PropertyName;
+                    }
+                };
+                dynamicObservableObject.Property1 = "newtest";
+
+                Assert.AreEqual(1, counter);
+                Assert.AreEqual(propertyName, "Property1");
+            }
+
+            [TestCase]
+            public void CorrectlyReturnsTheDefaultValue_WhenNotSet()
+            {
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                Assert.AreEqual(null, observableObject.GetValue<string>("Property1"));
+                Assert.AreEqual(0, observableObject.GetValue<int>("Property2"));
+                Assert.AreEqual(0F, observableObject.GetValue<float>("Property3"));
+                Assert.AreEqual(0M, observableObject.GetValue<decimal>("Property4"));
+                Assert.AreEqual(DateTime.MinValue, observableObject.GetValue<DateTime>("Property5"));
+                //
+                Assert.AreEqual(null, observableObject.GetValue<string>("Property1"));
+                Assert.AreEqual(null, observableObject.GetValue<int?>("Property2"));
+                Assert.AreEqual(null, observableObject.GetValue<float?>("Property3"));
+                Assert.AreEqual(null, observableObject.GetValue<decimal?>("Property4"));
+                Assert.AreEqual(null, observableObject.GetValue<DateTime?>("Property5"));
+            }
+
+            [TestCase]
+            public void RaisesAdvancedPropertyChangingEvents_WhenSetViaSetValueMethod()
+            {
+                var counter = 0;
+                var propertyName = default(string);
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Setting value via SetValue method.
+                observableObject.SetValue("Property1", "oldtest");
+                observableObject.PropertyChanging += (sender, e) =>
+                {
+                    AdvancedPropertyChangingEventArgs args = e as AdvancedPropertyChangingEventArgs;
+                    if (args != null)
+                    {
+                        counter++;
+                        propertyName = args.PropertyName;
+                    }
+                };
+                observableObject.SetValue("Property1", "newtest");
+
+                Assert.AreEqual(1, counter);
+                Assert.AreEqual(propertyName, "Property1");
+            }
+
+            [TestCase]
+            public void RaisesAdvancedPropertyChangedEvents_WhenSetViaDynamicProperty()
+            {
+                var counter = 0;
+                var propertyName = default(string);
+                var oldValue = default(object);
+                var newValue = default(object);
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Setting value via dynamic property.
+                dynamicObservableObject.Property1 = "oldtest";
+                observableObject.PropertyChanged += (sender, e) =>
+                {
+                    AdvancedPropertyChangedEventArgs args = e as AdvancedPropertyChangedEventArgs;
+                    if (args != null)
+                    {
+                        counter++;
+                        propertyName = args.PropertyName;
+                        oldValue = args.OldValue;
+                        newValue = args.NewValue;
+                    }
+                };
+                dynamicObservableObject.Property1 = "newtest";
+
+                Assert.AreEqual(1, counter);
+                Assert.AreEqual(propertyName, "Property1");
+                Assert.AreEqual(oldValue, "oldtest");
+                Assert.AreEqual(newValue, "newtest");
+            }
+
+            [TestCase]
+            public void RaisesAdvancedPropertyChangedEvents_WhenSetViaSetValueMethod()
+            {
+                var counter = 0;
+                var propertyName = default(string);
+                var oldValue = default(object);
+                var newValue = default(object);
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                // Setting value via dynamic property.
+                observableObject.SetValue("Property1", "oldtest");
+                observableObject.PropertyChanged += (sender, e) =>
+                {
+                    AdvancedPropertyChangedEventArgs args = e as AdvancedPropertyChangedEventArgs;
+                    if (args != null)
+                    {
+                        counter++;
+                        propertyName = args.PropertyName;
+                        oldValue = args.OldValue;
+                        newValue = args.NewValue;
+                    }
+                };
+                observableObject.SetValue("Property1", "newtest");
+
+                Assert.AreEqual(1, counter);
+                Assert.AreEqual(propertyName, "Property1");
+                Assert.AreEqual(oldValue, "oldtest");
+                Assert.AreEqual(newValue, "newtest");
+            }
+
+            [TestCase]
+            public void ThrowsArgumentExceptionWhenPropertyNameIsNullOrWhitespace_WhenSetViaSetValueMethod()
+            {
+                var observableObject = new CustomObject();
+                dynamic dynamicObservableObject = observableObject;
+
+                Assert.Throws<ArgumentException>(() => observableObject.SetValue(null, "test"));
+                Assert.Throws<ArgumentException>(() => observableObject.SetValue("", "test"));
+                Assert.Throws<ArgumentException>(() => observableObject.SetValue(" ", "test"));
+            }
+        }
+    }
+}
