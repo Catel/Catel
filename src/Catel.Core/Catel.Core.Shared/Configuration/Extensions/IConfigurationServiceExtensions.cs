a03759d77f8d260880986eeff156d32351ca2bee
Geert van Horrik <geert@catenalogic.com>
2016-04-19 10:19:15 +0200

    Added more extensions for the configuration service

--- src/Catel.Core/Catel.Core.Shared/Configuration/Extensions/IConfigurationServiceExtensions.cs
+++ src/Catel.Core/Catel.Core.Shared/Configuration/Extensions/IConfigurationServiceExtensions.cs
@@ -15,6 +15,66 @@ namespace Catel.Configuration
     public static class IConfigurationServiceExtensions
     {
         /// <summary>
+        /// Determines whether the specified value is available using <see cref="ConfigurationContainer.Local" />.
+        /// </summary>
+        /// <param name="configurationService">The configuration service.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>The configuration value.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="configurationService" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="key" /> is <c>null</c> or whitespace.</exception>
+        public static bool IsLocalValueAvailable(this IConfigurationService configurationService, string key)
+        {
+            Argument.IsNotNull("configurationService", configurationService);
+
+            return configurationService.IsValueAvailable(ConfigurationContainer.Local, key);
+        }
+
+        /// <summary>
+        /// Determines whether the specified value is available using <see cref="ConfigurationContainer.Roaming" />.
+        /// </summary>
+        /// <param name="configurationService">The configuration service.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>The configuration value.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="configurationService" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="key" /> is <c>null</c> or whitespace.</exception>
+        public static bool IsRoamingValueAvailable(this IConfigurationService configurationService, string key)
+        {
+            Argument.IsNotNull("configurationService", configurationService);
+
+            return configurationService.IsValueAvailable(ConfigurationContainer.Roaming, key);
+        }
+
+        /// <summary>
+        /// Initializes the value by setting the value to the <paramref name="defaultValue" /> if the value does not yet exist using <see cref="ConfigurationContainer.Local"/>.
+        /// </summary>
+        /// <param name="configurationService">The configuration service.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="configurationService" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="key" /> is <c>null</c> or whitespace.</exception>
+        public static void InitializeLocalValue(this IConfigurationService configurationService, string key, object defaultValue)
+        {
+            Argument.IsNotNull("configurationService", configurationService);
+
+            configurationService.InitializeValue(ConfigurationContainer.Local, key, defaultValue);
+        }
+
+        /// <summary>
+        /// Initializes the value by setting the value to the <paramref name="defaultValue" /> if the value does not yet exist using <see cref="ConfigurationContainer.Roaming"/>.
+        /// </summary>
+        /// <param name="configurationService">The configuration service.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="configurationService" /> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="key" /> is <c>null</c> or whitespace.</exception>
+        public static void InitializeRoamingValue(this IConfigurationService configurationService, string key, object defaultValue)
+        {
+            Argument.IsNotNull("configurationService", configurationService);
+
+            configurationService.InitializeValue(ConfigurationContainer.Roaming, key, defaultValue);
+        }
+
+        /// <summary>
         /// Gets the configuration value using <see cref="ConfigurationContainer.Local" />.
         /// </summary>
         /// <typeparam name="T">The type of the value to retrieve.</typeparam>
