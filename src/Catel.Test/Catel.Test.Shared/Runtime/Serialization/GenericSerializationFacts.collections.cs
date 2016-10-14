610a5733d1d8144fd25db2bf2d1cd420aeaf31e6
Geert van Horrik <geert@catenalogic.com>
2016-10-14 11:54:22 +0200

    Temporarily disable bson dictionary unit tests

--- src/Catel.Test/Catel.Test.Shared/Runtime/Serialization/GenericSerializationFacts.collections.cs
+++ src/Catel.Test/Catel.Test.Shared/Runtime/Serialization/GenericSerializationFacts.collections.cs
@@ -789,6 +789,16 @@ namespace Catel.Test.Runtime.Serialization
 
                 TestSerializationOnAllSerializers((serializer, config, description) =>
                 {
+                    var jsonSerializationConfig = config as JsonSerializationConfiguration;
+                    if (jsonSerializationConfig != null)
+                    {
+                        // Note: we don't support dictionaries yet for bson
+                        if (jsonSerializationConfig.UseBson)
+                        {
+                            return;
+                        }
+                    }
+
                     var deserializedObject = SerializationTestHelper.SerializeAndDeserialize(dictionary, serializer, config);
 
                     Assert.AreEqual(dictionary.Count, deserializedObject.Count, description);
