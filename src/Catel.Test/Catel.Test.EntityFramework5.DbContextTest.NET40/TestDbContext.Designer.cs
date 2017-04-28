58c5742646550e9c7836f86cde7e40fbcd9021db
Geert van Horrik <geert@catenalogic.com>
2017-04-28 15:40:33 +0200

    #1008 Drop support for .NET 4.0

--- src/Catel.Test/Catel.Test.EntityFramework5.DbContextTest.NET40/TestDbContext.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Source\Catel\src\Catel.Test\Catel.Test.EntityFramework5.DbContextTest.NET40\TestDbContext.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
--- src/Catel.Test/Catel.Test.EntityFramework5.ObjectContextTest.NET45/ModuleInitializer.cs
+++ src/Catel.Test/Catel.Test.EntityFramework5.ObjectContextTest.NET45/ModuleInitializer.cs
@@ -1,1 +1,28 @@
-﻿// Empty by design so it will never be overwritten by an update
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ModuleInitializer.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.Test
+{
+    /// <summary>
+    /// Class that gets called as soon as the module is loaded.
+    /// </summary>
+    /// <remarks>
+    /// This is made possible thanks to Fody.
+    /// </remarks>
+    public static class ModuleInitializer
+    {
+        #region Methods
+
+        /// <summary>
+        /// Initializes the module
+        /// </summary>
+        public static void Initialize()
+        {
+        }
+
+        #endregion
+    }
+}
