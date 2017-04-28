8247aa128ce67346788a0522f51f78cf7c0ab426
Geert van Horrik <geert@catenalogic.com>
2017-04-28 12:44:46 +0200

    Packages maintenance

--- src/GlobalSuppressions.cs
+++ src/GlobalSuppressions.cs
@@ -5,4 +5,8 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF0041:Set mutable dependency properties using SetCurrentValue.", Justification = "Not supported in UWP")]
-
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1010:Property '[Property]' must notify when value changes.", Justification = "Don't enforce this")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1011:Implement INotifyPropertyChanged.", Justification = "Don't enforce this")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1012:Notify that property '[Property]' changes..", Justification = "Don't enforce this")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1013:Use [CallerMemberName].", Justification = "Don't enforce this, base class doesn't neccessarily support this")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1015:Check if value is different before notifying.", Justification = "Don't enforce this, base class doesn't neccessarily support this")]
