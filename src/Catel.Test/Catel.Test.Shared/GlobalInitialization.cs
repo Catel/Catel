6f7ca36a567a45fb07d8a41953b9c02224d770c8
Geert van Horrik <geert@catenalogic.com>
2017-05-01 12:27:40 +0200

    Migrate all code from Catel.Extensions.EntityFramework to Orc.EntityFramework

--- src/Catel.Test/Catel.Test.Shared/GlobalInitialization.cs
+++ src/Catel.Test/Catel.Test.Shared/GlobalInitialization.cs
@@ -23,36 +23,5 @@ public class GlobalInitialization
 
         // Required since we do multithreaded initialization
         TypeCache.InitializeTypes(allowMultithreadedInitialization: false);
-
-#if NET && !SKIP_EF
-        using (var dbContext = new Catel.Test.EntityFramework5.DbContextTest.TestDbContextContainer())
-        {
-            dbContext.Database.CreateIfNotExists();
-
-            // Delete all data
-            var allOrders = (from x in dbContext.DbContextOrders
-                             select x).ToList();
-            foreach (var x in allOrders)
-            {
-                dbContext.DbContextOrders.Remove(x);
-            }
-
-            var allCustomers = (from x in dbContext.DbContextCustomers
-                                select x).ToList();
-            foreach (var x in allCustomers)
-            {
-                dbContext.DbContextCustomers.Remove(x);
-            }
-
-            var allProducts = (from x in dbContext.DbContextProducts
-                               select x).ToList();
-            foreach (var x in allProducts)
-            {
-                dbContext.DbContextProducts.Remove(x);
-            }
-
-            dbContext.SaveChanges();
-        }
-#endif
     }
 }
