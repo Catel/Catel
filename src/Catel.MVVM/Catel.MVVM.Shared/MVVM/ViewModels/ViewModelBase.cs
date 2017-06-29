54922e8a2cf4d7981d57f94330ef7e05043a852c
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2017-06-29 01:35:13 -0400

    (x) #892 ModelBase CancelEdit puts Model into dirty state

--- src/Catel.MVVM/Catel.MVVM.Shared/MVVM/ViewModels/ViewModelBase.cs
+++ src/Catel.MVVM/Catel.MVVM.Shared/MVVM/ViewModels/ViewModelBase.cs
@@ -988,6 +988,12 @@ namespace Catel.MVVM
                                             string.Join(", ", propertiesToSet), string.Join(", ", valuesToSet));
                                     }
 
+                                    
+                                    if (modelInfo.SupportIEditableObject)
+                                    {
+                                        (model as IEditableObject)?.BeginEdit();
+                                    }
+
                                     for (var index = 0; index < propertiesToSet.Length && index < valuesToSet.Length; index++)
                                     {
                                         try
@@ -1411,10 +1417,19 @@ namespace Catel.MVVM
 
             lock (_modelLock)
             {
+                bool isDirty = false;
+
                 foreach (var modelKeyValuePair in _modelObjects)
                 {
                     UninitializeModelInternal(modelKeyValuePair.Key, modelKeyValuePair.Value, ModelCleanUpMode.CancelEdit);
+                    var model = modelKeyValuePair.Value as IModel;
+                    if (model != null)
+                    {
+                        isDirty |= model.IsDirty;
+                    }
                 }
+
+                IsDirty = isDirty;
             }
 
             Log.Info("Canceled view model '{0}'", GetType());
@@ -1477,10 +1492,19 @@ namespace Catel.MVVM
             {
                 lock (_modelLock)
                 {
+                    bool isDirty = false;
                     foreach (KeyValuePair<string, object> modelKeyValuePair in _modelObjects)
                     {
                         UninitializeModelInternal(modelKeyValuePair.Key, modelKeyValuePair.Value, ModelCleanUpMode.EndEdit);
+
+                        var model = modelKeyValuePair.Value as IModel;
+                        if (model != null)
+                        {
+                            isDirty |= model.IsDirty;
+                        }
                     }
+
+                    IsDirty = isDirty;
                 }
 
                 await SavedAsync.SafeInvokeAsync(this);
