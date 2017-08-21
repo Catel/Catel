916cde2ca24832f5f73ddc51241dcbc0e03e6d3a
Geert van Horrik <geert@catenalogic.com>
2017-08-21 14:29:09 +0200

    Remove test code for MethodTimer

--- src/Catel.Core/Catel.Core.Shared/Argument.cs
+++ src/Catel.Core/Catel.Core.Shared/Argument.cs
@@ -39,7 +39,6 @@ namespace Catel
         /// <exception cref="System.ArgumentNullException"></exception>
         /// <exception cref="ArgumentNullException">If <paramref name="paramValue" /> is <c>null</c>.</exception>
         [DebuggerNonUserCode, DebuggerStepThrough]
-        [MethodTimer.Time]
         public static void IsNotNull(string paramName, object paramValue)
         {
             if (paramValue == null)
