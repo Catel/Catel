606e6c287d7140f2f54e2f0ac1f09a27d3009754
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2017-09-01 01:22:12 -0700

    Implement unit test for TypeCache deadlock

--- src/Catel.Test/Catel.Test.Shared/Reflection/TypeCacheFacts.cs
+++ src/Catel.Test/Catel.Test.Shared/Reflection/TypeCacheFacts.cs
@@ -7,6 +7,7 @@ namespace Catel.Test.Reflection
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Catel.Reflection;
     using NUnit.Framework;
 
@@ -90,5 +91,27 @@ namespace Catel.Test.Reflection
                 Assert.AreEqual(typeof(MySpecialClass), interfaces[0]);
             }
         }
+
+        [Test]
+        public void Does_Not_Enter_In_A_Deadlock()
+        {
+            TypeCache.InitializeTypes();
+            Assert.IsTrue(Task.WaitAll(new Task[] { LoadABAsync(), LoadACAsync() }, 5000));
+        }
+
+        static Task<Type> LoadACAsync()
+        {
+            return Task.Run(() => Type.GetType("A.AC, A, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+        }
+
+        static Task<Type> LoadABAsync()
+        {
+            return Task.Run(() => Type.GetType("A.AB, A, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+        }
     }
 }
+
+
+
+
+
