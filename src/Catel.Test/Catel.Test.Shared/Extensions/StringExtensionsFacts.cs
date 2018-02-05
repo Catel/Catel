ae95d3f0cd28659666055b0ff2c04f8ca58ae29e
Geert van Horrik <geert@catenalogic.com>
2018-02-05 20:44:49 +0100

    (*) #1159 Allow EqualsIgnoreCase / ContainsIgnoreCase / etc to accept null values (then the result should be false)

--- src/Catel.Test/Catel.Test.Shared/Extensions/StringExtensionsFacts.cs
+++ src/Catel.Test/Catel.Test.Shared/Extensions/StringExtensionsFacts.cs
@@ -9,35 +9,95 @@ namespace Catel.Test.Extensions
 {
     using NUnit.Framework;
 
+    [TestFixture]
     public class StringExtensionsFacts
     {
-        [TestFixture]
         public class TheSplitByCamelCaseMethod
         {
             [TestCase(null, null)]
             [TestCase("ThisIsATest", "This is a test")]
             [TestCase("IsIncluded", "Is included")]
             [TestCase("PropertyName", "Property name")]
-            public void SplitsCorrectly(string input, string expectedOutput)
+            public void SplitByCamelCase(string input, string expectedOutput)
             {
                 var output = input.SplitCamelCase();
 
                 Assert.AreEqual(expectedOutput, output);
             }
-        }
 
-        [TestFixture]
-        public class TheGetSlugMethod
-        {
             [TestCase("this.hEllO something", "thishEllOsomething", false)]
             [TestCase("this.hEllO something", "thishellosomething", true)]
             [TestCase("tesTß", "tesT", false)]
             [TestCase("tesTß", "test", true)]
             [TestCase("tesT\\*&$", "tesT", false)]
             [TestCase("tesT\\*&$", "test", true)]
-            public void ReturnsRightSlug(string input, string expectedOutput, bool lowercase)
+            public void GetSlug(string input, string expectedOutput, bool lowercase)
             {
                 var output = input.GetSlug(makeLowercase: lowercase);
+
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            [TestCase("HELLO", null, false)]
+            [TestCase(null, "HELLO", false)]
+            [TestCase("HELLO", "HELLO", true)]
+            [TestCase("HELLO", "hello", true)]
+            [TestCase("HELLO", "hello1", false)]
+            public void EqualsIgnoreCase(string input1, string input2, bool expectedOutput)
+            {
+                var output = input1.EqualsIgnoreCase(input2);
+
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            [TestCase("HELLO", null, false)]
+            [TestCase(null, "HELLO", false)]
+            [TestCase("HELLO", "HELL", true)]
+            [TestCase("HELLO", "HELLO", true)]
+            [TestCase("HELLO", "hello", true)]
+            [TestCase("HELLO", "hello1", false)]
+            public void ContainsIgnoreCase(string input1, string input2, bool expectedOutput)
+            {
+                var output = input1.ContainsIgnoreCase(input2);
+
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            [TestCase("HELLO", null, false)]
+            [TestCase(null, "HELLO", false)]
+            [TestCase("HELLO", "HELL", true)]
+            [TestCase("HELLO", "HELLO", true)]
+            [TestCase("HELLO", "hello", true)]
+            [TestCase("HELLO", "ello", false)]
+            public void StartsWithIgnoreCase(string input1, string input2, bool expectedOutput)
+            {
+                var output = input1.StartsWithIgnoreCase(input2);
+
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            [TestCase("HELLO", null, false)]
+            [TestCase(null, "HELLO", false)]
+            [TestCase("HELLO", "ELLO", true)]
+            [TestCase("HELLO", "HELLO", true)]
+            [TestCase("HELLO", "hello", true)]
+            [TestCase("HELLO", "hell", false)]
+            public void EndsWithIgnoreCase(string input1, string input2, bool expectedOutput)
+            {
+                var output = input1.EndsWithIgnoreCase(input2);
+
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            [TestCase("HELLO", null, -1)]
+            [TestCase(null, "HELLO", -1)]
+            [TestCase("HELLO", "ELLO", 1)]
+            [TestCase("HELLO", "HELLO", 0)]
+            [TestCase("HELLO", "hello", 0)]
+            [TestCase("HELLO", "hell", 0)]
+            public void IndexOfIgnoreCase(string input1, string input2, int expectedOutput)
+            {
+                var output = input1.IndexOfIgnoreCase(input2);
 
                 Assert.AreEqual(expectedOutput, output);
             }
