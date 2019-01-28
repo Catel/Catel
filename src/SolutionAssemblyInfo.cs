3e283aeebfdc64879f3124e16e41b47f23b7b40b
Geert van Horrik <geert@catenalogic.com>
2019-01-28 14:27:58 +0100

    Bump versions

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("CatenaLogic")]
-[assembly: AssemblyVersion("5.9.0")]
-[assembly: AssemblyFileVersion("5.9.0")]
-[assembly: AssemblyInformationalVersion("5.9.0-alpha.1")]
+[assembly: AssemblyVersion("5.10.0")]
+[assembly: AssemblyFileVersion("5.10.0")]
+[assembly: AssemblyInformationalVersion("5.10.0-alpha.1")]
 [assembly: AssemblyCopyright("Copyright © CatenaLogic 2010 - 2019")]
 
