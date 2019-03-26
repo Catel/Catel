6f6a63be375e0088df57795bc14c622a9919a67a
GreenKn1ght <heccu-kalter2@yandex.ru>
2019-03-27 00:11:50 +0300

    #1252 Catel.MVVM.Xamarin.Forms MessageBox showing without buttons

--- src/Catel.MVVM.Xamarin.Forms/Properties/Resources.Designer.cs
+++ src/Catel.MVVM.Xamarin.Forms/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Catel.Properties {
+namespace Catel.MVVM.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Catel.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Catel.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Catel.MVVM.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
