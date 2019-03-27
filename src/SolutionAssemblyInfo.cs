83877b30a99e67c350118709d0f3583484c3b919
Geert van Horrik <geert@catenalogic.com>
2019-03-27 20:12:01 +0100

    #1238 First try at multitargeting different UAP versions

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("CatenaLogic")]
 [assembly: AssemblyVersion("5.10.0")]
 [assembly: AssemblyFileVersion("5.10.0")]
-[assembly: AssemblyInformationalVersion("5.10.0-alpha.1")]
+[assembly: AssemblyInformationalVersion("5.10.0-beta.13")]
 [assembly: AssemblyCopyright("Copyright © CatenaLogic 2010 - 2019")]
 
