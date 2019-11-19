b50d8b7939b4dc1f5af0b649c9a1fa45009f7979
Geert van Horrik <geert@catenalogic.com>
2019-11-19 22:44:21 +0100

    Fix unit tests

--- src/Catel.Tests/Configuration/DynamicConfigurationFacts.cs
+++ src/Catel.Tests/Configuration/DynamicConfigurationFacts.cs
@@ -22,6 +22,7 @@ namespace Catel.Tests.Configuration
     {
         private const string ExpectedXml = "﻿<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
 "<DynamicConfiguration xmlns:ctl=\"http://schemas.catelproject.com\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n" +
+"  <ComplexSetting ctl:IsNull=\"true\" />\r\n" +
 "  <KeyX ctl:type=\"System.String\">Value X</KeyX>\r\n" +
 "  <KeyY ctl:type=\"System.String\">Value Y</KeyY>\r\n" +
 "  <KeyZ.SomeAddition ctl:type=\"System.String\">Value Z</KeyZ.SomeAddition>\r\n" +
