0bacc741c0d4249c78eef4810b8962c59199f8e3
Geert van Horrik <geert@catenalogic.com>
2020-09-09 22:09:19 +0200

    Test improvements

---
+++ src/Catel.Tests/ModuleInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Catel.Tests
+{
+    /// <summary>
+    /// Class that gets called as soon as the module is loaded.
+    /// </summary>
+    /// <remarks>
+    /// This is made possible thanks to Fody.
+    /// </remarks>
+    public static class ModuleInitializer
+    {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public static void Initialize()
+        {
+
+        }
+    }
+}
