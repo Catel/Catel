a35b45a6f8a81e43b97b19d65a1df5e2ed38a71c
Geert van Horrik <geert@catenalogic.com>
2024-11-19 12:53:20 +0100

    #2277 Add DispatcherTimerEx that uses IDispatcherService

---
+++ src/Catel.MVVM/Windows/Threading/DispatcherTimerEx.cs
@@ -0,0 +1,102 @@
+﻿namespace Catel.Windows.Threading
+{
+    using System;
+    using System.Timers;
+    using Catel.Services;
+
+    /// <summary>
+    /// Dispatcher timer that uses the <see cref="IDispatcherService"/> to dispatch its calls so it can be
+    /// used inside unit tests.
+    /// </summary>
+    public class DispatcherTimerEx
+    {
+        protected readonly IDispatcherService _dispatcherService;
+
+#pragma warning disable IDISP006 // Implement IDisposable
+        private readonly System.Timers.Timer _timer = new System.Timers.Timer();
+#pragma warning restore IDISP006 // Implement IDisposable
+
+        private readonly object _lockObject = new object();
+        private bool _isSubscribed;
+
+        public DispatcherTimerEx(IDispatcherService dispatcherService)
+        {
+            _dispatcherService = dispatcherService;
+
+            OnlyBeginInvokeIfRequired = true;
+        }
+
+        /// <summary>
+        /// Gets or sets the interval.
+        /// </summary>
+        public virtual TimeSpan Interval
+        {
+            get => TimeSpan.FromMilliseconds(_timer.Interval);
+            set => _timer.Interval = value.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// Gets or sets whether the timer is enabled.
+        /// </summary>
+        public virtual bool IsEnabled
+        {
+            get => _timer.Enabled;
+            set => _timer.Enabled = value;
+        }
+
+        /// <summary>
+        /// If set the <c>true</c>, the timer will pass <c>true</c> to <see cref="IDispatcherService.BeginInvoke(Action, bool)"/>.
+        /// </summary>
+        public virtual bool OnlyBeginInvokeIfRequired { get; set; }
+
+        /// <summary>
+        /// Occurs when the timer ticks.
+        /// </summary>
+        public event EventHandler<EventArgs>? Tick = default;
+
+        /// <summary>
+        /// Starts the timer.
+        /// </summary>
+        public virtual void Start()
+        {
+            lock (_lockObject)
+            {
+                if (!_isSubscribed)
+                {
+                    _timer.Elapsed += OnTimerElapsed;
+                    _isSubscribed = true;
+                }
+
+                _timer.Start();
+            }
+        }
+
+        /// <summary>
+        /// Stops the timer.
+        /// </summary>
+        public virtual void Stop()
+        {
+            lock (_timer)
+            {
+                _timer.Stop();
+
+                _timer.Elapsed += OnTimerElapsed;
+                _isSubscribed = false;
+            }
+        }
+
+        private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            RaiseEvent();
+        }
+
+        protected virtual void RaiseEvent()
+        {
+            var handler = Tick;
+            if (handler is not null)
+            {
+                _dispatcherService.BeginInvoke(() => handler(this, EventArgs.Empty), OnlyBeginInvokeIfRequired);
+            }
+        }
+    }
+}
---
+++ src/Catel.Tests/Windows/Threading/DispatcherTimerExFacts.cs
@@ -0,0 +1,90 @@
+﻿namespace Catel.Tests.Windows.Threading
+{
+    using System;
+    using System.Threading.Tasks;
+    using Catel;
+    using Catel.Services;
+    using Catel.Windows.Threading;
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DispatcherTimerExFacts
+    {
+        [Test]
+        public async Task Runs_Dispatcher_Methods_With_Mock_Async()
+        {
+            var dispatcherServiceMock = new Mock<IDispatcherService>();
+
+            var dispatcherTimerEx = new DispatcherTimerEx(dispatcherServiceMock.Object)
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+
+            dispatcherTimerEx.Tick += (sender, args) =>
+            {
+                // Dummy
+            };
+
+            using (new DisposableToken<DispatcherTimerEx>(dispatcherTimerEx,
+                (x) => x.Instance.Start(),
+                (x) => x.Instance.Stop()))
+            {
+                await Task.Delay(100);
+
+                dispatcherServiceMock.Verify(x => x.BeginInvoke(It.IsAny<Action>(), It.Is<bool>(y => y == true)), Times.AtLeastOnce);
+            }
+        }
+
+        [Test]
+        public async Task Runs_Dispatcher_Methods_Without_BeginInvokeIfRequired_With_Mock_Async()
+        {
+            var dispatcherServiceMock = new Mock<IDispatcherService>();
+
+            var dispatcherTimerEx = new DispatcherTimerEx(dispatcherServiceMock.Object)
+            {
+                Interval = TimeSpan.FromMilliseconds(50),
+                OnlyBeginInvokeIfRequired = false
+            };
+
+            dispatcherTimerEx.Tick += (sender, args) =>
+            {
+                // Dummy
+            };
+
+            using (new DisposableToken<DispatcherTimerEx>(dispatcherTimerEx,
+                (x) => x.Instance.Start(),
+                (x) => x.Instance.Stop()))
+            {
+                await Task.Delay(100);
+
+                dispatcherServiceMock.Verify(x => x.BeginInvoke(It.IsAny<Action>(), It.Is<bool>(y => y == false)), Times.AtLeastOnce);
+            }
+        }
+
+        [Test]
+        public async Task Runs_Dispatcher_Methods_With_Implementation_Async()
+        {
+            var dispatcherTimerEx = new DispatcherTimerEx(new DispatcherService(new DispatcherProviderService()))
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+
+            var invoked = false;
+
+            dispatcherTimerEx.Tick += (sender, args) =>
+            {
+                invoked = true;
+            };
+
+            using (new DisposableToken<DispatcherTimerEx>(dispatcherTimerEx,
+                (x) => x.Instance.Start(),
+                (x) => x.Instance.Stop()))
+            {
+                await Task.Delay(100);
+            }
+
+            Assert.That(invoked, Is.True);
+        }
+    }
+}
