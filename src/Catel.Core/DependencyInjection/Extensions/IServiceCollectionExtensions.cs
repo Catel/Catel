0d4c61bdb53317d61f4d3603ed21027e0591b5ac
Geert van Horrik <geert@catenalogic.com>
2025-02-27 09:25:05 +0100

    Add IServiceCollection and IServiceProvider extensions

---
+++ src/Catel.Core/DependencyInjection/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Catel
+{
+    using System.Linq;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class IServiceCollectionExtensions
+    {
+        public static bool IsRegistered<TService>(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.Any(x => x.ServiceType == typeof(TService));
+        }
+    }
+}
---
+++ src/Catel.Core/DependencyInjection/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Catel
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class IServiceProviderExtensions
+    {
+        public static bool IsRegistered<TService>(this IServiceProvider serviceProvider)
+        {
+            return IsRegistered(serviceProvider, typeof(TService));
+        }
+
+        public static bool IsRegistered(this IServiceProvider serviceProvider, Type serviceType)
+        {
+            var serviceChecker = serviceProvider.GetRequiredService<IServiceProviderIsService>();
+
+            return serviceChecker.IsService(serviceType);
+        }
+    }
+}
