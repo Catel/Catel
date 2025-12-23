e2865cc9a5ce73c44a6f8a8c65f934715153fba2
Geert van Horrik <geert@catenalogic.com>
2025-12-23 13:52:37 +0100

    Use NullLogger from msft

--- src/Catel.Core/Logging/LogManager.cs
+++ src/Catel.Core/Logging/LogManager.cs
@@ -4,6 +4,7 @@
     using Catel.IoC;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging.Abstractions;
 
     public static class LogManager
     {
@@ -47,30 +48,7 @@
             }
 
             // No fallback set, use null logger that is always available
-            return new NullLogger();
-        }
-
-        private class NullLogger : NullLogger<object>
-        {
-
-        }
-
-        private class NullLogger<T> : ILogger<T>
-        {
-            public IDisposable? BeginScope<TState>(TState state) 
-                where TState : notnull
-            {
-                return new DisposableToken(this, static (x) => { }, static (x) => { });
-            }
-
-            public bool IsEnabled(LogLevel logLevel)
-            {
-                return false;
-            }
-
-            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-            {
-            }
+            return new NullLogger<object>();
         }
     }
 }
