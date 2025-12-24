254d249896164c3c095cc156422caecf325a05a0
Geert van Horrik <geert@catenalogic.com>
2025-12-24 14:50:35 +0100

    Add LanguageHelper for static contexts

---
+++ src/Catel.Core/Helpers/LanguageHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Catel
+{
+    using System;
+    using Catel.IoC;
+    using Catel.Services;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class LanguageHelper
+    {
+        private static readonly Lazy<ILanguageService> LanguageServiceLazy = new(() =>
+        {
+            return IoCContainer.ServiceProvider.GetRequiredService<ILanguageService>();
+        });
+
+        public static string? GetString(string resourceName)
+        {
+            return LanguageServiceLazy.Value.GetString(resourceName);
+        }
+
+        public static string GetRequiredString(string resourceName)
+        {
+            return LanguageServiceLazy.Value.GetRequiredString(resourceName);
+        }
+    }
+}
