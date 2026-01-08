3685398386feb6f596e308ecb2fdf30a02fa0c27
Geert van Horrik <geert@catenalogic.com>
2026-01-08 10:21:37 +0100

    Allow string comparison for interfaces

--- src/Catel.Core/Reflection/Extensions/ReflectionExtensions.type.cs
+++ src/Catel.Core/Reflection/Extensions/ReflectionExtensions.type.cs
@@ -393,6 +393,25 @@
             ArgumentNullException.ThrowIfNull(type);
             ArgumentNullException.ThrowIfNull(interfaceType);
 
+            ArgumentNullException.ThrowIfNull(type);
+
+            var typeName = interfaceType.FullName;
+
+            foreach (var iface in type.GetInterfacesEx())
+            {
+                try
+                {
+                    if (iface.FullName?.Equals(typeName) ?? false)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    // Ignore
+                }
+            }
+
             return IsAssignableFromEx(interfaceType, type);
         }
 
