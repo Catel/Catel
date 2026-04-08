45db4356e459a5000a6836aaf4afc99516c9c890
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:49:36 +0200

    Add service collection extension for keyed services

--- src/Catel.Core/IoC/Extensions/IServiceCollectionExtensions.cs
+++ src/Catel.Core/IoC/Extensions/IServiceCollectionExtensions.cs
@@ -6,6 +6,27 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class IServiceCollectionExtensions
 {
+    /// <summary>
+    /// Registers a service as keyed singleton *and* as singleton, allowing the keyed singleton to be resolved
+    /// by enumeration as well.
+    /// </summary>
+    /// <remarks>
+    /// This method / idea comes from https://stackoverflow.com/questions/78220267/get-all-implementations-of-a-given-type-from-iserviceprovider-regardless-of-if-t
+    /// <para />
+    /// Use with care.
+    /// </remarks>
+    /// <typeparam name="TService"></typeparam>
+    /// <typeparam name="TImplementation"></typeparam>
+    /// <param name="serviceCollection"></param>
+    /// <param name="key"></param>
+    public static void AddKeyedAndDefaultSingleton<TService, TImplementation>(this IServiceCollection serviceCollection, string key)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        serviceCollection.AddKeyedSingleton<TService, TImplementation>(key);
+        serviceCollection.AddSingleton(sp => sp.GetRequiredKeyedService<TService>(key));
+    }
+
     public static void AddParentServiceProviderRegistrations(this IServiceCollection serviceCollection, IServiceProvider serviceProvider)
     {
         var serviceProviderServiceCollection = serviceProvider.GetServiceCollection();
