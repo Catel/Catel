02e3da335e24aa04456de01bcd53dcba8788da19
Geert van Horrik <geert@catenalogic.com>
2026-05-14 20:26:27 +0200

    Add InitializeAtStartup class

---
+++ src/Catel.Core/IoC/InitializeAtStartup.cs
@@ -0,0 +1,18 @@
+﻿namespace Catel.IoC;
+
+using System;
+
+public class InitializeAtStartup : IInitializeAtStartup
+{
+    private readonly Action _action;
+
+    public InitializeAtStartup(Action action)
+    {
+        _action = action;
+    }
+
+    public void Initialize()
+    {
+        _action();
+    }
+}
