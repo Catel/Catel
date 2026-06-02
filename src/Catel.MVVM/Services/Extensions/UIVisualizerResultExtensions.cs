17d0de6b538806c35c1f5c9a7ebc2619075d40ef
Geert van Horrik <geert@catenalogic.com>
2026-06-02 10:17:30 +0200

    Add GetRequiredViewModel extension method

--- src/Catel.MVVM/Services/Extensions/UIVisualizerResultExtensions.cs
+++ src/Catel.MVVM/Services/Extensions/UIVisualizerResultExtensions.cs
@@ -12,4 +12,16 @@ public static class UIVisualizerResultExtensions
 
         return result.Context.Data as TViewModel;
     }
+
+    public static TViewModel GetRequiredViewModel<TViewModel>(this UIVisualizerResult result)
+        where TViewModel : class, IViewModel
+    {
+        var vm = GetViewModel<TViewModel>(result);
+        if (vm is null)
+        {
+            throw new InvalidOperationException($"The result does not contain a view model of type {typeof(TViewModel).FullName}");
+        }
+
+        return vm;
+    }
 }
